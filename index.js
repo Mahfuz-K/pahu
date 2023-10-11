e0a2ce8bbc08f8320214cca8a4ec02b50ad583f7
mahfuz <mahfuzflamedestiny@gmail.com>
2023-10-11 14:16:30 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var hamburger = document.querySelector("#ham-icon");
+    hamburger.addEventListener("click", function () {
+      var ul = document.querySelector("nav");
+      console.log(ul.style.left);
+      if (ul.style.left == "" || ul.style.left == "-100%") {
+        ul.style.left = 0;
+        hamburger.classList.remove("fa", "fa-bars");
+        hamburger.classList.add("fa-solid", "fa-xmark");
+      } else {
+        ul.style.left = "-100%";
+        hamburger.classList.remove("fa-solid", "fa-xmark");
+        hamburger.classList.add("fa", "fa-bars");
+      }
+    });
+  });
